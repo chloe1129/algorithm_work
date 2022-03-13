d1ae5601553100e21535b7408ec77948678ce407
Chaehy <chlee7670@cau.ac.kr>
2022-03-13 22:35:12 +0900

    bronze3: 20220313

---
+++ Bronze/10950.py
@@ -0,0 +1,11 @@
+T=int(input())
+for i in range(1,T+1):
+    a,b = map(int, input().split())
+    print(a+b)
+
+# 파이썬의 map
+# map(적용시킬 함수, 적용할 값들)
+
+# 파이썬의 split
+# a, b, input().splite()
+# 하나씩, a 입력받고 b 입력받는다
