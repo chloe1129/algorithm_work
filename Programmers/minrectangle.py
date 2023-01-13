c57ab4d3ebaa03cc92ab09b14e0393767f104218
Chaehy <chlee7670@cau.ac.kr>
2023-01-13 00:14:52 -0800

    pro1: min rectangle edit

--- Programmers/minrectangle.py
+++ Programmers/minrectangle.py
@@ -1,15 +1,19 @@
 def solution(sizes):
-    answer = []
     a = []
     b = []
 
     for i in range(len(sizes)):
-        answer.append((lambda x,y: x*y)(sizes[i][0], sizes[i][1]))
-
         if sizes[i][0] > sizes[i][1]:
             temp = sizes[i][0]
             sizes[i][0] = sizes[i][1]
             sizes[i][1] = temp
+
+        '''
+        이렇게 이차원 배열에서의 for 문 활용
+        for a,b in sizes:
+            if a< b:
+                a,b = b,a
+        '''
 
         a.append(sizes[i][0])
         b.append(sizes[i][1])
@@ -18,5 +22,7 @@ def solution(sizes):
     b = max(b)
 
     return a*b
+
+    
 
 print(solution([[60, 50],[30, 70],[60, 30],[80, 40]]))
