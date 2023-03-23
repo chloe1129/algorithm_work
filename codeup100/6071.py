ba2886e1a68f3715da3da8e2eed367f543aa580b
Chaehy <chlee7670@cau.ac.kr>
2023-03-23 15:18:27 -0700

    codeup basic loop

---
+++ codeup100/6071.py
@@ -0,0 +1,7 @@
+a=1
+while a!=0:
+    n=int(input())
+    if n!=0:
+        print(n)
+    else: 
+        a=0
---
+++ codeup100/6073.py
@@ -0,0 +1,6 @@
+a = int(input())
+while a!=0:
+    print(a-1)
+    a=a-1
+    if a==0:
+        break
