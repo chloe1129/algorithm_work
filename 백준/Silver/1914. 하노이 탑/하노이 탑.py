e599ddbd5997167841fdcdb52e15f8e41f306ae4
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-03-30 00:15:30 -0700

    Update 하노이 탑.py

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,12 +1,20 @@
+# a: 옮길 원반이 있는 출발점
+# b: 원반을 옮길 도착점
+# c: 옮길 때 사용할 보조 기둥
+
 def f(n, a, b, c):
   if(n == 1):
-    print(a, c, sep = " ")
+    print(a, b, sep = " ")
+    return
   else:
+    # 원반 n-1개를 보조 기둥으로
     f(n-1, a, c, b)
-    f(1, a, b, c)
-    f(n-1, b, a, c)
+    print(a, b, sep = " ")
+    
+    # 보조 기둥의 원반 n-1을 목적지로 이동
+    f(n-1, c,b,a)
 
 n = int(input())
 print(2**n-1)
 if(n <= 20):
-  f(n, 1, 2, 3)
+  f(n, 1, 3,2)
