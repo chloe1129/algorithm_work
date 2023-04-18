43e1b59fa6225ebb17e7ee31830a7206fd325ca5
Chaehy <chlee7670@cau.ac.kr>
2023-04-18 16:06:44 -0700

    add: temp

---
+++ 백준/ZZ/10989.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input)
+num = []
+for _ in range(n):
+    num.append(input)
+
+print(num)
--- 백준/ZZ/9663_re.py
+++ 백준/ZZ/9663_re.py
@@ -1,2 +1,8 @@
 N = int(input())
 
+def backtrack(n):
+    for i in range(n):
+        if n == 1:
+            return 1
+        else:
+            
