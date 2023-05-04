fee90e234cff2de9150fe7712be66a66a421226a
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-05-04 00:22:41 -0700

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,9 @@
+def solution(strings, n):
+    answer = {}
+    strings = sorted(strings)
+    for i in range(len(strings)):
+        answer[strings[i]] = strings[i][n]
+    
+    a = dict(sorted(answer.items(), key=lambda item: item[1]))
+
+    return list(a.keys())
