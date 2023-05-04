dd5eab6d1e58c31ad7a8c98c79e3f68365a4d805
Chaehy <chlee7670@cau.ac.kr>
2023-05-03 23:13:27 -0700

    add: programmers level 1 three kids

--- Programmers/level 1/threekids.py
+++ Programmers/level 1/threekids.py
@@ -1,5 +1,14 @@
+import itertools
+
 def solution(number):
     answer = 0
+    a = [' '.join(str(i)) for i in number]
+    b = list(itertools.combinations(number, 3))
+
+    for i in b:
+        c = list(map(int, i))
+        if sum(c) == 0:
+            answer+=1
     return answer
 
-print(solution([-2, 3, 0, 2, -5]))
+print(solution([-3, -2, -1, 0, 1, 2, 3]	))
