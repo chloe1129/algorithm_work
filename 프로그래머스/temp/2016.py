228db278b5d49099520f1be15abae800c90d46a8
Chaehy <chlee7670@cau.ac.kr>
2023-05-08 22:54:16 -0700

    add: level1

---
+++ 프로그래머스/temp/2016.py
@@ -0,0 +1,15 @@
+import datetime
+
+def solution(a, b):
+    week = {
+            'Monday':"MON",
+            'Tuesday':"TUE",
+            'Wednesday':"WED",
+            'Thursday':"THU",
+            'Friday':"FRI",
+            'Saturday':"SAT",
+            'Sunday':"SUN"}
+    answer = datetime.date(2016, a, b)
+    return week[answer.strftime("%A")]
+
+print(solution(5,24))
