6613754b818b16a0a0171c824e4ed9885f9f2805
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-05-10 15:57:45 -0700

    [level 1] Title: 모의고사, Time: 3.13 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,21 @@
+import math
+
+
+def solution(answers):
+    answer = []
+    s = {
+        1: [1, 2, 3, 4, 5],
+        2: [2, 1, 2, 3, 2, 4, 2, 5],
+        3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    }
+    for i in range(len(s)):
+        if len(s) < len(answers):
+            s[i+1] = s[i+1]*(math.ceil(len(answers)/len(s[i+1])))
+        count = 0
+        for j in range(len(answers)):
+            if s[i+1][j] == answers[j]:
+                count += 1
+        answer.append(count)
+
+    m = max(answer)
+    return [i+1 for i, v in enumerate(answer) if v == m]
