8115e650d597b9eabe7a325900934212f2d952f1
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-05-10 15:25:37 -0700

    [level 1] Title: 소수 찾기, Time: 132.28 ms, Memory: 19.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    s = [True]*(n+1)
+    m = int(n**0.5)
+    for i in range(2, m+1):
+        if s[i] == True:
+            for j in range(i+i, n+1, i):
+                s[j] = False
+    answer = [i for i in range(2, n+1) if s[i] == True]
+    return len(answer)
