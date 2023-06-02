4687551d0fb21f6759edce3ccba8ebdf12b759a8
Chaehy <chlee7670@cau.ac.kr>
2023-06-01 22:02:00 -0700

    add: level1

--- 프로그래머스/temp/failure.py
+++ 프로그래머스/temp/failure.py
@@ -1,13 +1,26 @@
+
 def solution(N, stages):
     answer = []
 
     for i in range(1, N+1):
-        answer.append(list(map(lambda x: x-i, stages)))
+        a = 0
+        b = 0
+        for j in stages:
+            if j == i:
+                a += 1
+            if j >= i:
+                b += 1
+        if b == 0:
+            answer.append([i, 0])
+        else:
+            answer.append([i, float(a/b)])
 
-    for i in answer:
-        print(i)
+    a = sorted(answer, key=lambda x: x[1], reverse=True)
+    final = []
+    for i in a:
+        final.append(i[0])
 
-    return answer
+    return final
 
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
