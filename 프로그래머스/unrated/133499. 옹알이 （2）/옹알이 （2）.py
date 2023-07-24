7345b55f3eb1b5416d08b15828468506592ca1b7
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-07-24 16:24:49 -0700

    [unrated] Title: 옹알이 (2), Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,11 @@
+def solution(bab):
+    answer = 0
+    for i in bab:
+        word = str(i)
+        for j in ['aya', 'ye', 'woo', 'ma']:
+            if j*2 not in word:
+                word = word.replace(j, '_'*len(j))
+        if word == '_'*len(word):
+            answer += 1
+
+    return answer
