b91b1291e411294c647f5ea85eeb428921cce046
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-09-11 21:21:20 -0700

    [unrated] Title: 대충 만든 자판, Time: 0.49 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,21 @@
+def solution(keymap, targets):
+    result = []
+    count = {}
+    for lst in keymap:
+        for i, pad in enumerate(lst):
+            if (pad in count) and (count[pad] < i):
+                pass
+            else:
+                count[pad] = i
+    print(count)
+    for a in targets:
+        temp = 0
+        for b in a:
+            if b in count:
+                temp += (count[b]+1)
+            else:
+                temp = -1
+                break
+        result.append(temp)
+
+    return result
