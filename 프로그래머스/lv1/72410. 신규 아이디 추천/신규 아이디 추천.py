4745feb646d743234cef75cfec855d40c51ddd51
Chloe <68368104+chloe1129@users.noreply.github.com>
2023-09-26 23:49:46 -0700

    [level 1] Title: 신규 아이디 추천, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,43 @@
+import re
+
+
+def solution(new_id):
+    # level1
+    text = new_id.lower()
+
+    # level2
+    text = re.sub(r'[~!@#$%^&*\(\)=+\[\{\]\}:?,<>/]', '', text)
+
+    # level3
+    text = re.sub(r'\.+', '.', text)
+
+    # level 4
+    while (len(text) > 0):
+        if text[0] == '.' and text[-1] == '.':
+            text = text[1:-1]
+            break
+        elif text[-1] == '.':
+            text = text[:-1]
+            break
+        elif text[0] == '.':
+            text = text[1:]
+            break
+        else:
+            break
+
+    # level 5
+    if text == '':
+        text += 'a'
+
+    # level 6
+    if len(text) >= 16:
+        text = text[:15]
+        if text[-1] == '.':
+            text = text[:-1]
+
+    # level 7
+    while (len(text) < 3):
+        text += text[-1]
+        if len(text) > 2:
+            break
+    return text
