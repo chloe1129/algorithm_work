1e3e55b713b818b2753f1c5e3b8950f69e34521f
Chaehy <chlee7670@cau.ac.kr>
2023-10-08 20:37:21 +0900

    add: levle2

--- Softeer/level2_meetingroom.py
+++ Softeer/level2_meetingroom.py
@@ -9,57 +9,64 @@ room = [sys.stdin.readline().strip() for i in range(n)]
 room = sorted(room)
 
 data = {}
+result = {}
 for i in range(len(room)):
-    data[room[i]] = [[9, 18]]
+    data[room[i]] = [0]*18
+    result[room[i]] = []
 
-# {'avante':[[9,18]],
-# 'grandeur':[[9,18]],
-# 'sonata':[[9,18]]}
+for j in range(m):
+    name, st, et = map(str, sys.stdin.readline().strip().split())
+    st, et = int(st), int(et)
+    for k in range(st, et):
+        data[name][k] = 1
 
-# data['방이름'] -> [[9,18],[]..]
+# print(result)
+# print(data)
 
-for i in range(m):
-    # r : 방 이름
-    r, st, et = map(str, sys.stdin.readline().split())
-    l = 0
-    st = int(st)
-    et = int(et)
+i = 9
+r = 0
+temp = []
+while (i < 18 and r < len(room)):
+    # print('now info', room[r], i, data[room[r]][i])
+    if data[room[r]][i] == 0:
+        # print('what is in here', i, data[room[r]][i])
+        temp.append(i)
+    else:
+        if len(temp) > 0:
+            result[room[r]].append(temp)
+            temp = []
+    if i == 17 and len(temp) > 0:
+        result[room[r]].append(temp)
+        temp = []
+    i += 1
+    if i == 18:
+        i = 9
+        r += 1
 
-    while (l < len(data[r])):
-        room_time = data[r]
+# print(result)
 
-        if st <= room_time[l][1]:
-            if room_time[l][0] == st and room_time[l][1] == et:
-                data[r].remove([st, et])
-
-            elif room_time[l][0] < st and et < room_time[l][1]:
-                data[r].append([et, room_time[l][1]])
-                room_time[l] = [room_time[l][0], st]
-
-            elif room_time[l][0] < st and room_time[l][1] == et:
-                room_time[l] = [room_time[l][0], st]
-
-            elif room_time[l][0] == st and et < room_time[l][1]:
-                room_time[l] = [et, room_time[l][1]]
-
-            break
-
-        elif st > room_time[l][1]:
-            l += 1
-
-
-for i in range(len(data)):
-    data[room[i]] = list(sorted(data[room[i]]))
+for i in range(len(result)):
     print('Room '+room[i]+':')
-    if len(data[room[i]]) == 0:
+    if len(result[room[i]]) == 0:
         print('Not available')
     else:
-        print(str(len(data[room[i]]))+' available:')
+        print(str(len(result[room[i]])) + " available:")
+        for j in range(len(result[room[i]])):
+            # print(result[room[i]][j])
 
-        for j in range(len(data[room[i]])):
-            if data[room[i]][j][0] == 9:
-                data[room[i]][j][0] = '09'
-            print(str(data[room[i]][j][0])+'-'+str(data[room[i]][j][1]))
-
-    if 0 <= i < len(data)-1:
+            if len(result[room[i]][j]) == 1:
+                if result[room[i]][j][0] == 9:
+                    print('09-' +
+                          str(result[room[i]][j][0]+1))
+                else:
+                    print(str(result[room[i]][j][0]) + '-' +
+                          str(result[room[i]][j][0]+1))
+            else:
+                if result[room[i]][j][0] == 9:
+                    print('09-' +
+                          str(result[room[i]][j][0]+1))
+                else:
+                    print(str(result[room[i]][j][0]) + '-' +
+                          str(result[room[i]][j][len(result[room[i]][j])-1]+1))
+    if i < len(result)-1:
         print('-----')
