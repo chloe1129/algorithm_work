1c2d515dceab0f9770751a4dd79c115b572e933d
chloe1129 <chlee7670@cau.ac.kr>
2023-10-20 17:01:59 +0900

    fix: dir

--- 프로그래머스/lvl2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lvl2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -2,16 +2,13 @@ def solution(n):
     ans = 0
     if n == 0:
         ans+= 0
-    elif n == 1:
-        ans+= 1
 
-    else:
-        while(n>0):
-            if n%2 == 0:
-                # print('n is even',n,ans)
-                n=int(n/2)
-            else:
-                # print('n is odd',n,ans)
-                n=int((n-1)/2)
-                ans+=1
+    while(n>0):
+        if n%2 == 0:
+            # print('n is even',n,ans)
+            n=int(n/2)
+        else:
+            # print('n is odd',n,ans)
+            n=int((n-1)/2)
+            ans+=1
     return ans
