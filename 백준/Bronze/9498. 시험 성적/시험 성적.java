19d7a808b287e2a6180e65b68f3130fa802fd0b2
Chloe <68368104+chloe1129@users.noreply.github.com>
2024-01-09 20:52:09 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
-
-// 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int score = Integer.parseInt(bf.readLine());
-
-        if (score>= 90) bw.write('A');
-        else if (score >= 80) bw.write('B');
-        else if (score >= 70) bw.write('C');
-        else if (score >= 60) bw.write('D');
-        else bw.write('F');
-
-        bw.flush();
-
-        // 스트림 닫기
-        bw.close();
-    }
+ 
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 		int A = Integer.parseInt(br.readLine());
+        
+      
+        
+		if(A>=90) System.out.println("A");
+		else if(A>=80) System.out.println("B");
+		else if (A>=70) System.out.println("C");
+		else if(A>=60) System.out.println("D");
+		else System.out.println("F");
+	}
 }
