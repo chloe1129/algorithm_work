e626da65a8a3d304fc198e11111ad1cd51539b7f
Chloe <68368104+chloe1129@users.noreply.github.com>
2024-01-16 18:52:29 +0900

    [Bronze IV] Title: 세수정렬, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,31 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = bf.readLine();
-        StringTokenizer st = new StringTokenizer(s, " ");
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int [] arr = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<3;i++) {
+			System.out.print(arr[i] + " ");
+		}
 
-        int[] array2 = {a, b, c};
+	}
 
-        Arrays.sort(array2);
-
-        for(int i = 0; i<3; i++){
-            bw.write(array2[i]+" ");
-        }
-
-        bw.flush();
-        bw.close();
-    }
 }
